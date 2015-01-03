8f652298d86d884ec3a0a5e070b08fcab7c08c9b
thomas-henry-gomes <thomas.henry.gomes@gmail.com>
2015-01-03 14:35:27 +0100

    commit version 4.4 : recherche de youtubeur.

--- app/build/generated/source/buildConfig/release/com/youtubeurs/lite2/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/youtubeurs/lite2/BuildConfig.java
@@ -4,9 +4,9 @@
 package com.youtubeurs.lite2;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.youtubeurs.lite2";
-  public static final String BUILD_TYPE = "debug";
+  public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 13;
   public static final String VERSION_NAME = "";
